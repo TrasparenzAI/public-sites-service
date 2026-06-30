12ab3420c380d1049c92c194406554d728904dd2
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2026-06-30 15:48:29 +0200

    Passaggio a Spring @Autowired, introduzione gestione InvalidDataAccessApiUsageException, e aggiornamento a versione 0.7.2.

--- src/main/java/it/cnr/anac/transparency/companies/config/OpenApiConfiguration.java
+++ src/main/java/it/cnr/anac/transparency/companies/config/OpenApiConfiguration.java
@@ -33,7 +33,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
     info = @Info(title = "Public sites Service", 
-    version = "0.7.0",
+    version = "0.7.2",
     description = "Public Sites Service si occupa di gestire le informazioni principali relative "
         + "agli enti pubblici italiani ed in particolare i siti istituzionali"),
     servers = {
